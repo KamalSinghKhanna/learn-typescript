16c02a658bd7f37c55ca354e2a007d2c9b317722
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2024-05-18 11:19:57 +0530

    interface as a class

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,32 @@
-import { ChangeEvent, FormEvent, useState } from 'react'
-import './App.css'
-import Todo from './Todo'
 
-interface TodoType {
-  title: string;
-  description: string;
-  done: boolean;
-}
-function App() {
-  const [list, setList] = useState<TodoType[]>([])
-  const [text, setText] = useState<string>("")
-  const [des, setdes] = useState<string>("")
-  const handlesubmit = (e: FormEvent) =>{
-    e.preventDefault();
-    setList([...list, {
-      title:  text,
-      description: des,
-      done : false
-    }])
-    setText("")
-  } 
-  const handleChange = (e: ChangeEvent<HTMLInputElement> ) =>{
-    setText(e.target.value)
+const App = () => {
+  interface Person {
+    name: string;
+    age: number;
+    greet(phrase: string): void;
   }
-  return (
-    <>
-      <form onSubmit={handlesubmit}>
-        <input value={text} onChange={handleChange} type="text" placeholder="add new task..." />
-        <input value={des} onChange={(e)=> setdes(e.target.value)} type="text" placeholder="add new task..." />
-      <button type='submit'>submit</button>
-      </form>
 
-     <Todo list={list} />
-    </>
-  );
+  class Employee implements Person {
+    name: string;
+    age: number;
+
+    constructor(n: string, a: number) {
+      this.name = n;
+      this.age = a;
+    }
+
+    greet(phrase: string) {
+      console.log(`${phrase} ${this.name}`);
+    }
+  }
+  // Creating an Employee object
+  const employee = new Employee("John", 30);
+
+  // Accessing properties and calling method
+  console.log(employee.name); // Output: John
+  console.log(employee.age); // Output: 30
+  employee.greet("Hello"); // Output: Hello John
+  return <div>App</div>;
 }
 
 export default App
