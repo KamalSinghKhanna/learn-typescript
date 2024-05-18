81adef1fb38b75294feb61ed253f719713f88558
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2024-05-18 12:02:20 +0530

    Generics

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 
 const App = () => {
- enum Direction {
-    Up = "UP",
-    Down = "Down",
-    Left = "Left",
-    Right = 'Right'
- }
 
- function doSomething(keyPressed: Direction) {
-   // do something.
-   console.log(keyPressed)
- }
 
- doSomething(Direction.Left);
+function identity<T>(arg: T): T {
+  return arg;
+}
+
+const output1 = identity<string>("myString");
+const output2 = identity<number>(100);
+
+// console.log(output1, output2)
+
+function getFirstElement<T>(arr: T[]) {
+  return arr[0];
+}
+
+const el = getFirstElement(["kamal", "mukul"]);
+const el2 = getFirstElement<string>(["HarkiratSingh", 1]);
+
+console.log(el2.toLowerCase());
   return (
     <div>App</div>
   )
